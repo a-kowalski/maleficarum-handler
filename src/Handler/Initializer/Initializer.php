5474b315bfc8d7f2c6734199f7dcb056834fc66c
Adam Kowalski <adam.kowalski@miinto.com>
2017-08-21 15:07:56 +0200

    Add handler strategy initializer

---
+++ src/Handler/Initializer/Initializer.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This class carries ioc initialization functionality used by this component.
+ */
+declare (strict_types=1);
+
+namespace Maleficarum\Handler\Initializer;
+
+class Initializer {
+    /* ------------------------------------ Class Methods START ---------------------------------------- */
+
+    /**
+     * This method will initialize the entire package.
+     *
+     * @param array $opts
+     *
+     * @return string
+     */
+    static public function initialize(array $opts = []): string {
+        // load default builder if skip not requested
+        $builders = $opts['builders'] ?? [];
+        is_array($builders) or $builders = [];
+        if (!isset($builders['exception_handler']['skip'])) {
+            \Maleficarum\Ioc\Container::register('Maleficarum\Handler\Http\Strategy', function ($dep, $opts) {
+                $class = $opts['__class'];
+
+                /** @var \Maleficarum\Handler\Http\Strategy\AbstractStrategy $strategy */
+                $strategy = new $class();
+                if (isset($dep['Maleficarum\Response'])) {
+                    $strategy->setResponse($dep['Maleficarum\Response']);
+                }
+
+                return $strategy;
+            });
+        }
+
+        // return initializer name
+        return __METHOD__;
+    }
+
+    /* ------------------------------------ Class Methods END ------------------------------------------ */
+}
